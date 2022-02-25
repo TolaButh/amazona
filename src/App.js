02da1bda9f68eb04bff2723a90b89fb5f55c38de
Toal Buth <std.62122420127@ubru.ac.th>
2022-02-25 21:42:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import data from './data';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="grid-container">
+    <header className="row">
+        <div>
+            <a className="brand" href="index.html">amazona</a>
+        </div>
+        <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign</a>
+        </div>
+    </header>
+    <main>
+       <div className="row center">
+      {
+        data.products.map(product=>(
+          <div key={product._id} className="card">
+          <a href={`/product/${product._id}`}>
+             
+              <img className="medium" src={product.image} alt={product.name}/>
+          </a>
+          <div className="card-body">
+          <a href={`/product/${product._id}`}>
+              <h2>{product.name}</h2>
+              </a>
+              <div className="rating">
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+                  <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">
+                  ${product.price}
+              </div>
+          </div>
+      </div>
+
+     
+
+        ))
+      }
+       
+
+
+
+
+       </div>
+    </main>
+    <footer className="row center">
+        <div>All right resserved</div>
+      
+    </footer>
+</div>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,95 @@
+const data = {
+    products:[
+    {
+        _id:'1',
+        name: 'Nike Slim Shirt',
+        category: 'Shirts',
+        image:'/images/product-01.jpg',
+        price:120,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product'
+    },
+    {
+        _id:'2',
+        name: 'Adidas Fit Shirt',
+        category: 'Shirts',
+        image:'/images/product-02.jpg',
+        price:100,
+        brand: 'Adidas',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product'
+    },
+    {
+        _id:'3',
+        name: 'Jaiyka Slim Shirt',
+        category: 'Shirts',
+        image:'/images/product-03.jpg',
+        price:120,
+        brand: 'Jaiyka',
+        rating: 4.5,
+        numReviews: 14,
+        description: 'high quality product'
+    },
+    {
+        _id:'4',
+        name: 'Dad Slim Shirt',
+        category: 'Shirts',
+        image:'/images/product-05.jpg',
+        price:140,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product'
+    }
+    ,
+    {
+        _id:'5',
+        name: 'Chanjar Slim Shirt',
+        category: 'Shirts',
+        image:'/images/product-02.jpg',
+        price:190,
+        brand: 'Chanjar',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product'
+    },
+    {
+        _id:'6',
+        name: 'Dadas Slim Shirt',
+        category: 'Plants',
+        image:'/images/product-06.jpg',
+        price:130,
+        brand: 'Dadas',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product'
+    },
+    {
+        _id:'7',
+        name: 'Jinja Slim Shirt',
+        category: 'Pants',
+        image:'/images/product-7.jpg',
+        price:180,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product'
+    },
+    {
+        _id:'8',
+        name: 'Fila Slim Shirt',
+        category: 'Plants',
+        image:'/images/product-8.jpg',
+        price:100,
+        brand: 'Fila',
+        rating: 4.5,
+        numReviews: 15,
+        description: 'high quality product'
+    }
+]
+};
+
+export default data;
